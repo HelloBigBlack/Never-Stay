c157533677efee35d1dd60c3dde6256f3c715fc5
郑能量！ <1301691180@qq.com>
2019-06-14 14:00:59 +0800

    1.0 版本

--- main.js
+++ main.js
@@ -7,18 +7,20 @@ let win
 function createWindow () {
   // 创建浏览器窗口。
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 740,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    // 固定宽高
+    // resizable: false
   })
 
   // 加载index.html文件
   win.loadFile('index.html')
 
   // 打开开发者工具
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
